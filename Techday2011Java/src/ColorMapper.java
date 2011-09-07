3cef471c9af263a9e18f6dbcd6d28dfa46b0165d
cbreuel <cristiano.breuel@gmail.com>
2011-09-06 22:20:23 -0300

    Correção do exemplo Clojure e versão Java do mesmo.

---
+++ Techday2011Java/src/ColorMapper.java
@@ -0,0 +1,26 @@
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class ColorMapper {
+
+	private static String[] COLORS = {"white", "blue"};
+	
+	public static Map<String, String> mapLineColors(String[] values) {
+		Map<String, String> retVal = new HashMap<String, String>();
+		int i = 0;
+		for (String value: values) {
+			retVal.put(value, COLORS[i % COLORS.length]);
+			i ++;
+		}
+		return retVal;
+	}
+	
+	public static void main(String[] args) {
+		
+		String[] values = {"Clojure", "Java", "Scala", "Visual Basic"};
+		
+		System.out.println(mapLineColors(values));
+	}
+	
+}
--- examples.clj
+++ examples.clj
@@ -1,4 +1,4 @@
 (def items ["Clojure" "Java" "Scala" "Visual Basic"])
 (def colors (cycle ["white" "blue"]))
-(interleave items colors)
+(zipmap items colors)
 
