8d4bec213f954c7e9089f4a02fd5dab21500f248
cbreuel <cristiano.breuel@gmail.com>
2011-09-10 03:08:18 -0300

    Improved Java integration example

--- examples.clj
+++ examples.clj
@@ -2,3 +2,11 @@
 (def colors ["white" "blue"])
 (zipmap items (cycle colors))
 
+
+(import '(javax.swing JFrame JLabel))
+(def frm (JFrame.))
+(def lbl (JLabel. "Hello, Clojure!"))
+(doto frm (. add lbl) (. pack) (. show))
+
+(doto (JFrame.) (. add (JLabel. "Hello, Clojure!")) (. pack) (. show))
+
