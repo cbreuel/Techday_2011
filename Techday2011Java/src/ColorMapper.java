34765e1512c2423bff8ed4dfb0adc210612e6cca
cbreuel <cristiano.breuel@gmail.com>
2011-09-12 00:53:42 -0300

    Finished Clojure slides and examples

--- Techday2011Java/src/ColorMapper.java
+++ Techday2011Java/src/ColorMapper.java
@@ -1,5 +1,16 @@
+import java.awt.Component;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+
+import javax.swing.JComponent;
+import javax.swing.JLabel;
+import javax.swing.JTextArea;
 
 
 public class ColorMapper {
@@ -21,6 +32,51 @@ public class ColorMapper {
 		String[] values = {"Clojure", "Java", "Scala", "Visual Basic"};
 		
 		System.out.println(mapLineColors(values));
+	}
+	
+	public void hofExamples() {
+		
+		new Thread(new Runnable() {
+			public void run() {
+				System.out.println("Hello, Thread");
+			}
+		});
+		
+		
+		String[] dnaBases = {"AG", "GGATAC", "CTG"};
+		Arrays.sort(dnaBases, new Comparator<String>() {
+			public int compare(String arg0, String arg1) {
+				int l1 = arg0.length(), l2 = arg1.length();
+				if (l1 > l2) {
+					return 1;
+				} else if (l1 < l2) {
+					return -1;
+				} else {
+					return 0;
+				}
+			}
+		});
+		
+		
+//		JComponent cmp = new JTextArea();
+//		cmp.addKeyListener(new KeyListener() {
+//			public void keyTyped(KeyEvent arg0) {
+//				// TODO Auto-generated method stub
+//			}
+//			public void keyReleased(KeyEvent arg0) {
+//				// TODO Auto-generated method stub
+//			}
+//			public void keyPressed(KeyEvent arg0) {
+//				// TODO Auto-generated method stub
+//			}
+//		});
+		
+		int[] iarray = {1, 2, 3, 4};
+		int result = 0;
+		for (int i: iarray) {
+			result += i;
+		}
+
 	}
 	
 }
--- examples.clj
+++ examples.clj
@@ -1,7 +1,33 @@
-(def items ["Clojure" "Java" "Scala" "Visual Basic"])
-(def colors ["white" "blue"])
-(zipmap items (cycle colors))
+; Higher-order functions
 
+; Runnable
+(. (new Thread #(println "Hello, Thread")) start)
+
+; Comparator
+(def dna-bases ["AG" "GGATAC" "CTG"])
+(sort (comparator (fn [a b] (< (count a) (count b)))) dna-bases)
+(sort-by #(count %) dna-bases)
+
+; Iteration
+(def iarray [1 2 3 4])
+
+
+; Lazy sequences
+
+(def *items* ["Clojure" "Java" "Scala" "Visual Basic"])
+(def *colors* ["white" "blue"])
+(defn map-line-colors [items colors]
+  (zipmap items (cycle colors)))
+(map-line-colors *items* *colors*)
+
+(defn map-line-colors-2 [items colors]
+  (loop [retval {} itm items clr colors]
+    (if (empty? itm) 
+      retval
+      (recur (assoc retval (first itm) (first clr)) (next itm) (next clr)))))
+
+
+; Java integration
 
 (import '(javax.swing JFrame JLabel))
 (def frm (JFrame.))
